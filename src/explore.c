9d79c7e1bf095ba1576b795545e9934c9f1d1591
Sau P <sau@yaaama.xyz>
2026-01-27 15:20:20 +0000

    feat: Recursive file searches

--- src/explore.c
+++ src/explore.c
@@ -65,3 +65,137 @@ void anu_stack_destroy (anuStack* s) {
   free(s->items);
   s = NULL;
 }
+
+int anu_open_dir(char* dir_path, DIR** out);
+int anu_files_in_path(DIR** dir, struct dirent** filelist_out);
+
+/* Video extensions */
+const char* VIDEO_EXTENSIONS[] = {
+    "3g2", "3gp",  "amv",  "asf", "avi", "f4a",  "f4b", "f4p", "f4v", "flv",
+    "flv", "gifv", "m4p",  "m4v", "m4v", "mkv",  "mng", "mod", "mov", "mp2",
+    "mp4", "mpe",  "mpeg", "mpg", "mpv", "mxf",  "nsv", "ogg", "ogv", "qt",
+    "rm",  "roq",  "rrc",  "svi", "vob", "webm", "wmv", "yuv", NULL};
+
+int anu_open_dir (char* dir_path, DIR** out) {
+
+  *out = opendir(dir_path);
+
+  if (*out == NULL) {
+    perror("Could not open directory.");
+    return 1;
+  }
+  /* printf("Opened directory: `%s`!\n", dir_path); */
+  return 0;
+}
+
+/* Check extension of filename */
+int extension_is_supported (const char* filename) {
+  assert(filename);
+  const char* dot = strrchr(filename, '.');
+
+  if (!dot || dot == filename) {
+    return 0;
+  }
+
+  char file_ext_lower[8];
+
+  /* Skip over the dot... */
+  ++dot;
+
+  strncpy(file_ext_lower, dot, 7);
+  file_ext_lower[7] = '\0';
+
+  /* Lowercase all the characters */
+  for (int i = 0; file_ext_lower[i]; i++) {
+    file_ext_lower[i] = (char)tolower(file_ext_lower[i]);
+  }
+
+  /* Search if extension is within array */
+  for (int i = 0; VIDEO_EXTENSIONS[i] != NULL; i++) {
+    if (strcmp(file_ext_lower, VIDEO_EXTENSIONS[i]) == 0) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+/* TODO Resolve tilde into absolute path */
+int anu_resolve_tilde (char* path) { return 0; }
+
+int anu_recursive_filewalk (char* searchp) {
+
+  /* Initialise first directory we will explore */
+  anuDirJob dirjob;
+  strncpy(dirjob.path, searchp, ANU_MAX_PATH_LEN);
+
+  /* Stack containing directories to visit */
+  anuStack dirstack;
+  anu_stack_init(&dirstack, 50, sizeof(anuDirJob));
+  anu_stack_push(&dirstack, &dirjob);
+
+  /* Temp var to hold the directory we are currently in */
+  anuDirJob currjob;
+  /* Directory stream */
+  DIR* dir;
+  /* Dir entry */
+  struct dirent* dp;
+  /* Stat buffer */
+  struct stat statb;
+  /* Return value of calling stat on file */
+  int stat_return = 0;
+  /* Path of current file */
+  char fullpath[ANU_MAX_PATH_LEN];
+  /* Files found counter */
+  size_t files_found = 0;
+
+  while (anu_stack_pop(&dirstack, &currjob)) {
+
+    /* Open directory for reading */
+    if (anu_open_dir(currjob.path, &dir)) {
+      printf("Could not open directory: %s\n", currjob.path);
+      continue;
+    };
+
+    while ((dp = readdir(dir)) != NULL) {
+
+      /* Skip over '.' and '..' */
+      if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0) {
+        continue;
+      }
+
+      if (snprintf(fullpath, ANU_MAX_PATH_LEN, "%s/%s", currjob.path,
+                   dp->d_name) >= ANU_MAX_PATH_LEN) {
+        continue;
+      }
+      stat_return = stat(fullpath, &statb);
+      /* Handle stat errors here... */
+      if (stat_return) {
+        printf("We've encountered an error with this file: %s", fullpath);
+        continue;
+      }
+
+      /* If its a directory */
+      if (S_ISDIR(statb.st_mode)) {
+        /* printf("Directory found: %s\n", fullpath); */
+        strncpy(dirjob.path, fullpath, ANU_MAX_PATH_LEN);
+        anu_stack_push(&dirstack, &dirjob);
+      }
+
+      /* Else if its a regular file */
+      else if (S_ISREG(statb.st_mode)) {
+        if (extension_is_supported(fullpath)) {
+          /* printf("%s :: %zu\n", fullpath, files_found); */
+          printf("%s\n", fullpath);
+          ++files_found;
+        }
+      }
+    }
+
+    closedir(dir);
+  }
+
+  printf("Files found: %zu\n", files_found);
+
+  anu_stack_destroy(&dirstack);
+  return 0;
+}
--- src/explore.h
+++ src/explore.h
@@ -1,25 +1,31 @@
 #ifndef EXPLORE_H_
 #define EXPLORE_H_
 
+#include <dirent.h>
 #include <inttypes.h>
 #include <stdbool.h>
+#include <stddef.h>
+
 typedef char* u8;
 
 typedef struct media_info {
   bool fill_later;
-} mediainfo;
+} media_info;
 
 typedef struct media_flags {
   bool fill_later;
-} mediaflags;
+} media_flags;
 
 typedef struct file_entry {
   u8 path;
   u8 folder;
-  mediainfo info;
-  mediaflags flags;
-  long long size;
+  media_info info;
+  media_flags flags;
+  size_t size;
 
 } file_entry;
 
+
+int anu_open_dir (char* dir_path, DIR** out);
+int anu_recursive_filewalk (char* searchp);
 #endif  // EXPLORE_H_
