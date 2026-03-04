4d846caaf648f7e7f958f62094cc94440602cf6b
Sau P <sau@yaaama.xyz>
2026-03-04 11:39:37 +0000

    Make: Auto-build important targets

--- config.mk
+++ config.mk
@@ -2,38 +2,52 @@
 V ?= 0
 ifeq ($(V),1)
 		Q :=
+		ECHO_V := @echo
 else
 		Q := @
+		ECHO_V := @true
 endif
 
 CC ?= clang
 
-
 # --- C Flags ---
 # Development
-DEV_FLAGS := -ggdb \
--O0 \
--g3 \
--Wno-unused-function \
--Wno-unused-parameter \
--Wno-unused-variable \
--Wold-style-definition \
--fno-omit-frame-pointer \
--fno-optimize-sibling-calls \
--ftrapv \
--fstack-protector-all -Wstack-protector
+DEV_FLAGS := -ggdb3 -Og \
+-Wformat=2 \
+-Wno-unused-function -Wno-unused-parameter -Wno-unused-variable -Wno-missing-prototypes \
+-fno-omit-frame-pointer -fno-optimize-sibling-calls \
+-Wnull-dereference \
+-Wstack-protector -fstack-protector-strong \
+-fstack-clash-protection -fcf-protection
 
+# Compiler Specific
+COMPILER_CFLAGS :=
+COMPILER_LDFLAGS :=
+CLANG_EXTRA_SANS :=
+
+# Clang
 ifeq (${CC}, clang)
-	DEV_FLAGS += -fextend-variable-liveness -Wno-incompatible-pointer-types-discards-qualifiers
+	COMPILER_CFLAGS += -fextend-variable-liveness -Wno-incompatible-pointer-types-discards-qualifiers -Wthread-safety
+	COMPILER_CFLAGS += -Wcast-qual -Warray-bounds-pointer-arithmetic -Wassign-enum
+	COMPILER_CFLAGS += -D_FORTIFY_SOURCE=3 -flto=thin
+	COMPILER_LDFLAGS += -flto=thin
+# Clang extra sanitizers (Applied in Makefile if ASAN=1)
+	CLANG_EXTRA_SANS := -fsanitize=integer,implicit-conversion,local-bounds
 endif
 ifeq (${CC}, gcc)
-	DEV_FLAGS += -Wno-discarded-qualifiers
+	COMPILER_CFLAGS += -Wno-discarded-qualifiers -fanalyzer
+	COMPILER_CFLAGS += --param analyzer-bb-explosion-factor=50
+	COMPILER_CFLAGS += --param analyzer-max-enodes-per-program-point=200
+	COMPILER_CFLAGS += -Wanalyzer-too-complex
+	COMPILER_CFLAGS += -D_FORTIFY_SOURCE=3 -flto
+	COMPILER_LDFLAGS += -flto
 endif
 
 # Release Build
 RELEASE_FLAGS := -O2 -Wmissing-prototypes
-
+# Profile Build
 PROFILE_FLAGS := -O2 -g3 -fno-omit-frame-pointer -fno-optimize-sibling-calls
+
 
 # --- DEVELOPMENT TOGGLES ---
 # Appended to CPP_FLAGS:
