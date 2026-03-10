55212ca049661c4c567ace9d00decab19f0b51d4
Sau P <sau@yaaama.xyz>
2026-03-10 22:23:19 +0000

    log.c: Remove unused code

--- lib/log.c
+++ lib/log.c
@@ -54,17 +54,9 @@ static void stdout_callback (log_event *ev) {
   buf[strftime(buf, sizeof(buf), "%H:%M:%S", ev->time)] = '\0';
 
 #ifdef LOG_USE_COLOR
-
-#    ifdef LOG_USE_FUNC_NAME
-  fprintf(ev->udata, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:%s:\x1b[0m ", buf,
-          level_colors[ev->level], level_strings[ev->level], ev->file, ev->line,
-          ev->func);
-#    else
   fprintf(ev->udata, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ", buf,
           level_colors[ev->level], level_strings[ev->level], ev->file,
           ev->line);
-#    endif
-
 #else
   fprintf(ev->udata, "%s %-5s %s:%d: ", buf, level_strings[ev->level], ev->file,
           ev->line);
