6a7d5fe55974b9896273661e2e467d7eb3a77685
Sau P <sau@yaaama.xyz>
2026-09-14 22:19:32 +0100

    refactor(explore): Move fourcc code into new file
    - The fourcc logic was cluttering up the code and it can be made into a
    standalone header.

--- src/explore.c
+++ src/explore.c
@@ -18,6 +18,7 @@
 
 #include "config.h"
 #include "defs.h"
+#include "fourcc.h"
 #include "kvec.h"
 #include "log.h"
 #include "util.h"
@@ -43,89 +44,6 @@ static ALWAYS_INLINE int anu_cmp_str_lexicographic (const void *restrict a, cons
   return strcmp(*(const char *const *) a, *(const char *const *) b);
 }
 
-/**
- *  Declarations of static functions
- */
-static int handle_path_pointing_to_file(char *path, anu_file_vec *files_out) _nonnull_all_;
-/* END OF DECLARATIONS */
-
-#define fourcc_code(a, b, c, d) \
-  ((uint32_t) (a) | ((uint32_t) (b) << 8) | ((uint32_t) (c) << 16) | ((uint32_t) (d) << 24))
-
-/*
- * X Macro Table for Video Extensions, 4CC codes and string representations
- * Format: X(Enum Identifier, char1, char2, char3, char4, lowercase, UPPERCASE)
- */
-WARNING_PUSH
-DISABLE_WARNING_UNUSED_ALL
-
-#define VIDEO_EXTENSIONS_TABLE                            \
-  X(ANU_EXT_4CC_3G2, '3', 'g', '2', ' ', "3g2", "3G2")    \
-  X(ANU_EXT_4CC_3GP, '3', 'g', 'p', ' ', "3gp", "3GP")    \
-  X(ANU_EXT_4CC_AMV, 'a', 'm', 'v', ' ', "amv", "AMV")    \
-  X(ANU_EXT_4CC_ASF, 'a', 's', 'f', ' ', "asf", "ASF")    \
-  X(ANU_EXT_4CC_AVI, 'a', 'v', 'i', ' ', "avi", "AVI")    \
-  X(ANU_EXT_4CC_F4A, 'f', '4', 'a', ' ', "f4a", "F4A")    \
-  X(ANU_EXT_4CC_F4B, 'f', '4', 'b', ' ', "f4b", "F4B")    \
-  X(ANU_EXT_4CC_F4P, 'f', '4', 'p', ' ', "f4p", "F4P")    \
-  X(ANU_EXT_4CC_F4V, 'f', '4', 'v', ' ', "f4v", "F4V")    \
-  X(ANU_EXT_4CC_FLV, 'f', 'l', 'v', ' ', "flv", "FLV")    \
-  X(ANU_EXT_4CC_GIFV, 'g', 'i', 'f', 'v', "gifv", "GIFV") \
-  X(ANU_EXT_4CC_M4P, 'm', '4', 'p', ' ', "m4p", "M4P")    \
-  X(ANU_EXT_4CC_M4V, 'm', '4', 'v', ' ', "m4v", "M4V")    \
-  X(ANU_EXT_4CC_MKV, 'm', 'k', 'v', ' ', "mkv", "MKV")    \
-  X(ANU_EXT_4CC_MNG, 'm', 'n', 'g', ' ', "mng", "MNG")    \
-  X(ANU_EXT_4CC_MOD, 'm', 'o', 'd', ' ', "mod", "MOD")    \
-  X(ANU_EXT_4CC_MOV, 'm', 'o', 'v', ' ', "mov", "MOV")    \
-  X(ANU_EXT_4CC_MP2, 'm', 'p', '2', ' ', "mp2", "MP2")    \
-  X(ANU_EXT_4CC_MP4, 'm', 'p', '4', ' ', "mp4", "MP4")    \
-  X(ANU_EXT_4CC_MPE, 'm', 'p', 'e', ' ', "mpe", "MPE")    \
-  X(ANU_EXT_4CC_MPEG, 'm', 'p', 'e', 'g', "mpeg", "MPEG") \
-  X(ANU_EXT_4CC_MPG, 'm', 'p', 'g', ' ', "mpg", "MPG")    \
-  X(ANU_EXT_4CC_MPV, 'm', 'p', 'v', ' ', "mpv", "MPV")    \
-  X(ANU_EXT_4CC_MXF, 'm', 'x', 'f', ' ', "mxf", "MXF")    \
-  X(ANU_EXT_4CC_NSV, 'n', 's', 'v', ' ', "nsv", "NSV")    \
-  X(ANU_EXT_4CC_OGG, 'o', 'g', 'g', ' ', "ogg", "OGG")    \
-  X(ANU_EXT_4CC_OGV, 'o', 'g', 'v', ' ', "ogv", "OGV")    \
-  X(ANU_EXT_4CC_QT, 'q', 't', ' ', ' ', "qt", "QT")       \
-  X(ANU_EXT_4CC_RM, 'r', 'm', ' ', ' ', "rm", "RM")       \
-  X(ANU_EXT_4CC_ROQ, 'r', 'o', 'q', ' ', "roq", "ROQ")    \
-  X(ANU_EXT_4CC_RRC, 'r', 'r', 'c', ' ', "rrc", "RRC")    \
-  X(ANU_EXT_4CC_SVI, 's', 'v', 'i', ' ', "svi", "SVI")    \
-  X(ANU_EXT_4CC_VOB, 'v', 'o', 'b', ' ', "vob", "VOB")    \
-  X(ANU_EXT_4CC_WEBM, 'w', 'e', 'b', 'm', "webm", "WEBM") \
-  X(ANU_EXT_4CC_WMV, 'w', 'm', 'v', ' ', "wmv", "WMV")    \
-  X(ANU_EXT_4CC_YUV, 'y', 'u', 'v', ' ', "yuv", "YUV")
-
-/* Enum of 4cc codes */
-#define X(id, c1, c2, c3, c4, lower, upper) id = fourcc_code(c1, c2, c3, c4),
-
-typedef enum { VIDEO_EXTENSIONS_TABLE } ANU_VIDEO_4CC;
-
-#undef X
-
-/* Array of 4cc codes for videos */
-#define X(id, c1, c2, c3, c4, lower, upper) id,
-
-static const uint32_t video_4ccs[] = {/* NOLINT (unused-const-variable) */
-                                      VIDEO_EXTENSIONS_TABLE};
-#undef X
-
-/* Array of lowercase strings for video extensions */
-#define X(id, c1, c2, c3, c4, lower, upper) lower,
-static const char *video_exts_lower[] = {VIDEO_EXTENSIONS_TABLE};
-#undef X
-
-/* Array of uppercase strings for video extensions */
-#define X(id, c1, c2, c3, c4, lower, upper) upper,
-static const char *video_exts_upper[] = {VIDEO_EXTENSIONS_TABLE};
-#undef X
-
-static const int VIDEO_EXTENSION_MAX_LENGTH = 4;
-static const int VIDEO_EXTENSION_MIN_LENGTH = 2;
-
-WARNING_POP
-
 /* Check extension of filename */
 int anu_path_extension_supported (char *path) {
   assert(path);
@@ -149,7 +67,7 @@ int anu_path_extension_supported (char *path) {
       break;
     }
     /* Reached 5th character, extension is too long for a 4CC code */
-    if (i == VIDEO_EXTENSION_MAX_LENGTH) {
+    if (i == ANU_VIDEO_EXT_MAX_LEN) {
       return 0;
     }
 
@@ -157,26 +75,13 @@ int anu_path_extension_supported (char *path) {
   }
 
   /* If i was less than two characters */
-  if (i < VIDEO_EXTENSION_MIN_LENGTH) {
+  if (i < ANU_VIDEO_EXT_MIN_LEN) {
     return 0;
   }
 
-  uint32_t ext_4cc = fourcc_code(bytes[0], bytes[1], bytes[2], bytes[3]);
+  uint32_t ext_4cc = ANU_4CC_MAKE(bytes[0], bytes[1], bytes[2], bytes[3]);
 
-/* Match against supported formats */
-/*  X outputs just the enum id followed by a comma */
-#define X(id, c1, c2, c3, c4, lower, upper) id,
-/* Force expansion of our X macro inside the IN_SET macro */
-#define EXPAND_IN_SET(...) IN_SET(__VA_ARGS__)
-  /* Call IN_SET with the macro table */
-  int is_supported = EXPAND_IN_SET(
-      ext_4cc,
-      VIDEO_EXTENSIONS_TABLE 0x00000000 /* Dummy value to absorb the trailing comma (4cc code will never be 0) */
-  );
-
-#undef EXPAND_IN_SET
-#undef X
-  return is_supported;
+  return anu_4cc_is_valid(ext_4cc);
 }
 
 /**
---
+++ src/fourcc.h
@@ -0,0 +1,102 @@
+#ifndef FOURCC_H_
+#define FOURCC_H_
+
+#include <stdint.h>
+
+/* Length Macros */
+#define ANU_VIDEO_EXT_MAX_LEN 4
+#define ANU_VIDEO_EXT_MIN_LEN 2
+
+/* Helper Macros for Prefixing */
+#define __ANU_4CC_GLUE(a, b) a##b
+#define __ANU_4CC_JOIN(a, b) __ANU_4CC_GLUE(a, b)
+#define __ANU_4CC_PREFIX ANU_EXT_4CC_
+
+#define ANU_4CC_MAKE(a, b, c, d) \
+  ((uint32_t) (a) | ((uint32_t) (b) << 8) | ((uint32_t) (c) << 16) | ((uint32_t) (d) << 24))
+
+#define ANU_VID_EXT_MAX_LEN 4
+#define ANU_VID_EXT_MIN_LEN 2
+
+#define ANU_VIDEO_EXT_TABLE                   \
+  X(3G2, '3', 'g', '2', ' ', "3g2", "3G2")    \
+  X(3GP, '3', 'g', 'p', ' ', "3gp", "3GP")    \
+  X(AMV, 'a', 'm', 'v', ' ', "amv", "AMV")    \
+  X(ASF, 'a', 's', 'f', ' ', "asf", "ASF")    \
+  X(AVI, 'a', 'v', 'i', ' ', "avi", "AVI")    \
+  X(F4A, 'f', '4', 'a', ' ', "f4a", "F4A")    \
+  X(F4B, 'f', '4', 'b', ' ', "f4b", "F4B")    \
+  X(F4P, 'f', '4', 'p', ' ', "f4p", "F4P")    \
+  X(F4V, 'f', '4', 'v', ' ', "f4v", "F4V")    \
+  X(FLV, 'f', 'l', 'v', ' ', "flv", "FLV")    \
+  X(GIFV, 'g', 'i', 'f', 'v', "gifv", "GIFV") \
+  X(M4P, 'm', '4', 'p', ' ', "m4p", "M4P")    \
+  X(M4V, 'm', '4', 'v', ' ', "m4v", "M4V")    \
+  X(MKV, 'm', 'k', 'v', ' ', "mkv", "MKV")    \
+  X(MNG, 'm', 'n', 'g', ' ', "mng", "MNG")    \
+  X(MOD, 'm', 'o', 'd', ' ', "mod", "MOD")    \
+  X(MOV, 'm', 'o', 'v', ' ', "mov", "MOV")    \
+  X(MP2, 'm', 'p', '2', ' ', "mp2", "MP2")    \
+  X(MP4, 'm', 'p', '4', ' ', "mp4", "MP4")    \
+  X(MPE, 'm', 'p', 'e', ' ', "mpe", "MPE")    \
+  X(MPEG, 'm', 'p', 'e', 'g', "mpeg", "MPEG") \
+  X(MPG, 'm', 'p', 'g', ' ', "mpg", "MPG")    \
+  X(MPV, 'm', 'p', 'v', ' ', "mpv", "MPV")    \
+  X(MXF, 'm', 'x', 'f', ' ', "mxf", "MXF")    \
+  X(NSV, 'n', 's', 'v', ' ', "nsv", "NSV")    \
+  X(OGG, 'o', 'g', 'g', ' ', "ogg", "OGG")    \
+  X(OGV, 'o', 'g', 'v', ' ', "ogv", "OGV")    \
+  X(QT, 'q', 't', ' ', ' ', "qt", "QT")       \
+  X(RM, 'r', 'm', ' ', ' ', "rm", "RM")       \
+  X(ROQ, 'r', 'o', 'q', ' ', "roq", "ROQ")    \
+  X(RRC, 'r', 'r', 'c', ' ', "rrc", "RRC")    \
+  X(SVI, 's', 'v', 'i', ' ', "svi", "SVI")    \
+  X(VOB, 'v', 'o', 'b', ' ', "vob", "VOB")    \
+  X(WEBM, 'w', 'e', 'b', 'm', "webm", "WEBM") \
+  X(WMV, 'w', 'm', 'v', ' ', "wmv", "WMV")    \
+  X(YUV, 'y', 'u', 'v', ' ', "yuv", "YUV")
+
+/* Generate the Enum */
+#define X(id, c1, c2, c3, c4, lower, upper) \
+  __ANU_4CC_JOIN(__ANU_4CC_PREFIX, id) = ANU_4CC_MAKE(c1, c2, c3, c4),
+
+typedef enum { ANU_VIDEO_EXT_TABLE } ANU_VIDEO_4CC;
+
+#undef X
+
+/*
+ * Compile-time counting trick employed here
+ * We define X to just output "+ 1".
+ * The macro expands to (0 + 1 + 1 + 1...)
+ */
+#define X(id, c1, c2, c3, c4, lower, upper) (+1)
+#define ANU_VIDEO_EXT_COUNT (0 ANU_VIDEO_EXT_TABLE)
+#undef X
+
+/*
+ * Struct definition for an extension.
+ */
+typedef struct anu_ext_info {
+  char lower[5];
+  char upper[5];
+  unsigned char _padding[2];
+  ANU_VIDEO_4CC fourcc;
+} anu_ext_info;
+
+static inline __attribute__((const)) int anu_4cc_is_valid (const uint32_t fourcc_code) {
+  switch (fourcc_code) {
+    /* Define X to build the case statements: */
+#define X(id, c1, c2, c3, c4, lower, upper) case __ANU_4CC_JOIN(__ANU_4CC_PREFIX, id):
+
+    ANU_VIDEO_EXT_TABLE
+
+#undef X /* Undefine X so it doesn't leak out */
+    return 1;
+
+    /* Return 0 (false) if it does not match any extension */
+    default:
+      return 0;
+  }
+}
+
+#endif  // FOURCC_H_
