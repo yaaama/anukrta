8b9f2f1500a4c412f0afe394eaecad5e35650c7e
Sau P <sau@yaaama.xyz>
2026-09-22 23:01:52 +0100

    fix(video): Erroneous enum syntax

--- src/video.h
+++ src/video.h
@@ -35,7 +35,7 @@ typedef struct ak_vreader {
   int video_stream_idx;
 } ak_vreader;
 
-enum AK_FLAG_ENUM u32 { AK_LAV_SUPPS_DEC_GRAY = (1 << 0) };
+enum AK_FLAG_ENUM : u32 { AK_LAV_SUPPS_DEC_GRAY = (1 << 0) } ;
 
 /**
  * Checks if the linked libavcodec was compiled with certain features.
